d29fa4501956b979f326efad7977b097c77b08f5
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-13 23:51:48 -0300

    count

---
+++ simple-projects/count/script.js
@@ -0,0 +1,19 @@
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const count = document.getElementById('count');
+
+const dec = () => {
+    count.innerHTML = parseInt(count.innerHTML) - 1;
+}
+
+const inc = () => {
+    count.innerHTML = parseInt(count.innerHTML) + 1;
+}
+
+const teste = document.getElementById('teste');
+
+teste.style.color = 'red';
+
+
+
+
