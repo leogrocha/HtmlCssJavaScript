e7812361cef395ebf55e187edf2d324dfe0ad772
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-04-13 21:20:12 -0300

    Create Simple Projects

---
+++ simple-projects/sumOfTheNumbers/script.js
@@ -0,0 +1,5 @@
+let numberOne = prompt('Inform the number one');
+let numberTwo = prompt('Inform the number two');
+
+let result = parseInt(numberOne) + parseInt(numberTwo);
+document.getElementById('result').innerHTML = result;
