199126daff0efbfd3dfaa759887427e576b9729f
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-05-18 11:40:56 -0300

    objeto10

---
+++ js/objeto/objeto10.js
@@ -0,0 +1,208 @@
+const liquidez_balanco = [
+    {
+        "identificacao": {
+            "cnpj": "34.536.749/0001-70",
+            "nome_empresarial": "ECTARE PAY SERVICOS DE GESTAO DE PAGAMENTOS S/A"
+        },
+        "liquidez_indices": {
+            "imobilizacao_patrimonio_social": {
+                "indice": 0.04574443398294763,
+                "situacao": "otimo"
+            },
+            "indice_endividamento_geral": {
+                "indice": 1.094893817130752,
+                "situacao": "atencao"
+            },
+            "indice_liquidez_corrente": {
+                "indice": 1.106871705058769,
+                "situacao": "atencao"
+            },
+            "indice_liquidez_geral": {
+                "indice": 0.908564983149337,
+                "situacao": "ruim"
+            },
+            "indice_liquidez_imediata": {
+                "indice": 0.38031691531656775,
+                "situacao": "pessimo"
+            },
+            "indice_liquidez_seca": {
+                "indice": 1.106871705058769,
+                "situacao": "atencao"
+            },
+            "indice_solvencia_geral": {
+                "indice": 0.9133305753982354,
+                "situacao": "atencao"
+            },
+            "participacao_capital_terceiros": {
+                "indice": 11.538094369437413,
+                "situacao": "otimo"
+            }
+        },
+        "liquidez_variaveis": {
+            "ativo": [
+                {
+                    "T01": 1301904.37
+                },
+                {
+                    "T02": 3579042.96
+                },
+                {
+                    "T03": 9688509.75
+                },
+                {
+                    "T04": 9696366.76
+                }
+            ],
+            "ativo_circulante": [
+                {
+                    "T01": 1269279.08
+                },
+                {
+                    "T02": 3547132.09
+                },
+                {
+                    "T03": 9657113.3
+                },
+                {
+                    "T04": 9665684.73
+                }
+            ],
+            "ativo_nao_circulante": [
+                {
+                    "T01": 32625.29
+                },
+                {
+                    "T02": 31910.87
+                },
+                {
+                    "T03": 31396.45
+                },
+                {
+                    "T04": 30682.03
+                }
+            ],
+            "disponibilidades": [
+                {
+                    "T01": 321980.35
+                },
+                {
+                    "T02": 382766.01
+                },
+                {
+                    "T03": 5110801.13
+                },
+                {
+                    "T04": 2478593.14
+                }
+            ],
+            "estoques": [
+                {
+                    "T01": 0.0
+                },
+                {
+                    "T02": 0.0
+                },
+                {
+                    "T03": 0.0
+                },
+                {
+                    "T04": 0.0
+                }
+            ],
+            "imobilizado": [
+                {
+                    "T01": 27405.29
+                },
+                {
+                    "T02": 26690.87
+                },
+                {
+                    "T03": 25976.45
+                },
+                {
+                    "T04": 25262.03
+                }
+            ],
+            "obrigacoes_a_longo_prazo": [
+                {
+                    "T01": 1190000.66
+                },
+                {
+                    "T02": 1190000.66
+                },
+                {
+                    "T03": 1190000.66
+                },
+                {
+                    "T04": 1190000.66
+                }
+            ],
+            "passivo_circulante": [
+                {
+                    "T01": 466929.82
+                },
+                {
+                    "T02": 2891623.76
+                },
+                {
+                    "T03": 9136655.24
+                },
+                {
+                    "T04": 9313289.03
+                }
+            ],
+            "passivo_nao_circulante": [
+                {
+                    "T01": 1190000.66
+                },
+                {
+                    "T02": 1190000.66
+                },
+                {
+                    "T03": 1190000.66
+                },
+                {
+                    "T04": 1190000.66
+                }
+            ],
+            "patrimonio_liquido": [
+                {
+                    "T01": 355026.11
+                },
+                {
+                    "T02": 502581.46
+                },
+                {
+                    "T03": 638146.15
+                },
+                {
+                    "T04": 806922.93
+                }
+            ],
+            "realizavel_a_longo_prazo": [
+                {
+                    "T01": 0.0
+                },
+                {
+                    "T02": 0.0
+                },
+                {
+                    "T03": 0.0
+                },
+                {
+                    "T04": 0.0
+                }
+            ]
+        },
+        "periodo": "01/01/2020 a 31/12/2020"
+    }
+];
+
+class liquidez{
+    constructor(liquidez_balanco){
+        this.identificacao = liquidez_balanco[0].identificacao;
+    }
+}
+
+const indices = new liquidez(liquidez_balanco);
+console.log(indices.identificacao.nome_empresarial);
