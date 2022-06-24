ea0c4587d3c46ec33bfd4888e55d92a89da4adad
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-06-24 00:00:51 -0300

    card

---
+++ card/index.js
@@ -0,0 +1,8 @@
+const documentos = [
+    {id: 1, titulo_documento: 'documento_com_foto_frente', descricao_documento: 'documento_com_foto_frente'},
+    {id: 2, titulo_documento: 'documento_com_foto_verso', descricao_documento: 'documento_com_foto_verso'},
+    {id: 3, titulo_documento: 'comprovante_residencia', descricao_documento: 'comprovante_residencia'},
+    {id: 4, titulo_documento: 'certidao_casamento', descricao_documento: 'certidao_casamento'},
+    {id: 5, titulo_documento: 'outros'},
+]
+
--- programmingLogic/bigO/tempoExponencial.js
+++
@@ -1,8 +0,0 @@
-const fibonacci = (num) => {
-    if(num <= 1)
-      return num;
-    else
-      return fibonacci(num - 2) + fibonacci(num - 1)  
-  }
-  
-  console.log(fibonacci(8));
