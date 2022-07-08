3e6840e289c5bff3b51b4279e07f7c69cc42d23a
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-07-08 11:36:59 -0300

    finish formulario trocarAba

--- js/formularioTrocaAba/script.js
+++ js/formularioTrocaAba/script.js
@@ -1,29 +1,85 @@
+let tipopessoa = "";
+
+function verificandoTipoPessoa() {
+    const documento = '135.694.866-99';
+    //const documento = "26.246.306/0001-16";
+
+    if (documento.length === 14){
+        tipopessoa = "PF";
+        document.querySelector('.tipopessoapj').style.display = 'none';
+    }
+    else{
+        tipopessoa = "PJ";
+        document.querySelector('.tipopessoapf').style.display = 'none';
+    }
+}
+
+window.onload = function () {
+    verificandoTipoPessoa();
+}
+
 function trocaAbaCadastro(aba) {
-    [].forEach.call(document.querySelectorAll(".opcaoCadastro"), function (el) {
-        el.classList.remove("active");
-    });
-    document.getElementById(aba).className += ' active';
+    console.log(aba);
 
-    var dadosGerais = document.querySelector('.dadosGerais');
-    var quadroEndereco = document.querySelector('.quadroEndereco');
-    var quadroDadosBancarios = document.querySelector('.quadroDadosBancarios');
+    if (tipopessoa === "PF") {
 
-    if(aba === 'opcao-dados-gerais'){
-        dadosGerais.style.display = 'block';
-        quadroEndereco.style.display = 'none';
-        quadroDadosBancarios.style.display = 'none';
-    } else if(aba === 'opcao-dados-endereco'){
-        dadosGerais.style.display = 'none';
-        quadroEndereco.style.display = 'block';
-        quadroDadosBancarios.style.display = 'none';
-    } else if (aba === 'opcao-dados-bancarios') {
-        dadosGerais.style.display = 'none';
-        quadroEndereco.style.display = 'none';
-        quadroDadosBancarios.style.display = 'block';
+        [].forEach.call(document.querySelectorAll(".opcaoCadastro_pf"), function (el) {
+            el.classList.remove("active");
+        });
+        document.getElementById(aba).className += ' active';
+
+        var dadosGerais = document.querySelector('.dadosGerais_pf');
+        var quadroEndereco = document.querySelector('.quadroEndereco_pf');
+        var quadroDadosBancarios = document.querySelector('.quadroDadosBancarios_pf');
+
+        console.log(aba);
+
+        if (aba === 'opcao-dados-gerais_pf') {
+            dadosGerais.style.display = 'inherit';
+            quadroEndereco.style.display = 'none';
+            quadroDadosBancarios.style.display = 'none';
+        } else if (aba === 'opcao-dados-endereco_pf') {
+            dadosGerais.style.display = 'none';
+            quadroEndereco.style.display = 'inherit';
+            quadroDadosBancarios.style.display = 'none';
+        } else if (aba === 'opcao-dados-bancarios_pf') {
+            dadosGerais.style.display = 'none';
+            quadroEndereco.style.display = 'none';
+            quadroDadosBancarios.style.display = 'inherit';
+        }
+    } else if (tipopessoa === "PJ") {
+
+        [].forEach.call(document.querySelectorAll(".opcaoCadastro_pj"), function (el) {
+            el.classList.remove("active");
+        });
+        document.getElementById(aba).className += ' active';
+
+        var dadosGerais_pj = document.querySelector('.dadosGerais_pj');
+        var quadroEndereco_pj = document.querySelector('.quadroEndereco_pj');
+        var quadroDadosBancarios_pj = document.querySelector('.quadroDadosBancarios_pj');
+        var dadosRepresentante_rep = document.querySelector('.dadosRepresentante_rep');
+
+        if (aba === 'opcao-dados-gerais_pj') {
+            dadosGerais_pj.style.display = 'inherit';
+            quadroEndereco_pj.style.display = 'none';
+            quadroDadosBancarios_pj.style.display = 'none';
+            dadosRepresentante_rep.style.display = 'none';
+        } else if (aba === 'opcao-dados-endereco_pj') {
+            dadosGerais_pj.style.display = 'none';
+            quadroEndereco_pj.style.display = 'inherit';
+            quadroDadosBancarios_pj.style.display = 'none';
+            dadosRepresentante_rep.style.display = 'none';
+        } else if (aba === 'opcao-dados-bancarios_pj') {
+            dadosGerais_pj.style.display = 'none';
+            quadroEndereco_pj.style.display = 'none';
+            quadroDadosBancarios_pj.style.display = 'inherit';
+            dadosRepresentante_rep.style.display = 'none';
+        } else if (aba === 'opcao-dados-representante_rep') {
+            dadosGerais_pj.style.display = 'none';
+            quadroEndereco_pj.style.display = 'none';
+            quadroDadosBancarios_pj.style.display = 'none';
+            dadosRepresentante_rep.style.display = 'inherit';
+        }
     }
 
-    
-
-    
-    
 }
